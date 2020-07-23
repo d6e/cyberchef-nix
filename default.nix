93ac9771d80bd0c3c0ab6767de746a9bee0f4274
Danielle Jenkins <git@d6e.io>
2020-07-22 18:15:11 -0700

    Fix formatting.

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 { project ? import ./nix {} }:
 {
-cyberchef = project.pkgs.stdenv.mkDerivation rec {
-  name = "cyberchef";
-  version = "9.21.0";
-  src = project.pkgs.fetchzip {
-    url = "https://gchq.github.io/CyberChef/CyberChef_v${version}.zip";
-    stripRoot=false;
-    sha256 = "1y4z8fmy377wzzhrf7c6amglx7cl52hzpflwa6669y8s4d16bd6g";
+  cyberchef = project.pkgs.stdenv.mkDerivation rec {
+    name = "cyberchef";
+    version = "9.21.0";
+    src = project.pkgs.fetchzip {
+      url = "https://gchq.github.io/CyberChef/CyberChef_v${version}.zip";
+      stripRoot = false;
+      sha256 = "1y4z8fmy377wzzhrf7c6amglx7cl52hzpflwa6669y8s4d16bd6g";
+    };
+    builder = ./builder.sh;
+    coreutils = project.pkgs.coreutils;
   };
-  builder = ./builder.sh;
-  coreutils = project.pkgs.coreutils;
-};
-ci = (import ./nix {}).ci;
+  ci = (import ./nix {}).ci;
 }
